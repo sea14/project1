3e93f849ee3cda4e5defcc226754b1892ab081fc
Sharon <pianocat14@gmail.com>
2013-03-02 23:32:27 -0500

    second commit. not perfect, but very tired and need sleep. will hopefully be awesome by end of tomorrow.

--- team/urls.py
+++ team/urls.py
@@ -4,5 +4,9 @@ from django.conf.urls.defaults import patterns, url
 from team import views
 
 urlpatterns = patterns('',
-	url(^$', views.home, name='team_home'),
+	url(r'^$', views.home, name='team_home'),
+	#url(r'^team/$', views.teamList, name = 'team_team_list'),
+	#url(r'^member/$', views.memberList, name = 'team_member_list'),
+	url(r'^team/(?P<pk>\d+)$', views.team, name='team_team'),
+	url(r'^member/(?P<pk>\d+)$', views.member, name='team_member'),
 	)
--- team/views.py
+++ team/views.py
@@ -1,18 +1,22 @@
 # Create your views here.
 from django.shortcuts import render
 from team.models import Team, Member
+from django.shortcuts import render, get_object_or_404, redirect, render_to_response
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def home(request):
 	context = {
 	'member_count': Member.objects.count(),
 	'team_count': Team.objects.count(),
 }
-	return render(request, "base.html", context)
+	return render(request, "team/base.html", context)
 
-def member(request):
-	member = Member.objects.order_by('?')[0]
+def member(request, pk):
+	#member = Member.objects.order_by('?')[0]
+	course = get_object_or_404(Course, id=pk)
 	return render(request, "team/member.html", {'member': member})
 
-def team(request):
+def team(request, pk):
 	team = Team.objects.order_by('?')[0]
-	return render(request, "team/team.html", {'team'; team})
+	team = get_object_or_404(Team, id=pk)
+	return render(request, "team/team.html", {'team': team})
