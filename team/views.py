8abe0426eecd2ef0542446826ab2495725f7d34e
Sharon <pianocat14@gmail.com>
2013-03-16 22:32:58 -0400

    got member template to cooperate

--- team/views.py
+++ team/views.py
@@ -5,15 +5,14 @@ from django.shortcuts import render, get_object_or_404, redirect, render_to_resp
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def home(request):
-	context = {
-	'member_count': Member.objects.count(),
-	'team_count': Teams.objects.count(),
+        context = {
+        'member_count': Member.objects.count(),
+        'team_count': Teams.objects.count(),
 }
-	return render(request, "team/home.html", context)
+        return render(request, "team/home.html", context)
 
 def member(request, pk):
-	member = Member.objects.all()
-	#member = get_object_or_404(Member, id=pk)
+	member = get_object_or_404(Member, id=pk)
 	return render(request, "team/member.html", {'member': member})
 
 def teams(request, pk):
@@ -25,5 +24,5 @@ def teamsList(request):
 	return render(request, 'team/teamsList.html', {"teams": teams})
 
 def memberList(request):
-	member_List = member.objects.all()
+	member_List = Member.objects.all()
 	return render(request, 'team/member_list.html', {"members": members})
