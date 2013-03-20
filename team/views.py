285275e0a8169303e8fdc0f158377d746903330c
Sharon <pianocat14@gmail.com>
2013-03-20 00:00:56 -0400

    some small changes to logo, added team table and new slideshow to teams

--- team/views.py
+++ team/views.py
@@ -16,11 +16,13 @@ def member(request, pk):
 	member = get_object_or_404(Member, id=pk)
 	return render(request, "team/member.html", {'member': member})
 
+def teams(request, pk):
+        teams = get_object_or_404(Teams, id=pkd)
+        return render(request, "team/team.html", {'teams': teams})
 
 def teams(request):
     context = {
         'memberCount': Member.objects.all(),
         'teamsInfo' : Teams.objects.all(),
-        'teamsCount' : Teams.objects.count()
     }
     return render (request, "team/team.html", context)
